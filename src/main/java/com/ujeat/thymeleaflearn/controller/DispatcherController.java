449e13f82e425ef4c241331ba36134e7f76789c3
wanggang <wanggang225@163.com>
2017-07-26 14:32:48 +0800

    初始化导入

--- src/main/java/com/ujeat/thymeleaflearn/controller/DispatcherController.java
+++ src/main/java/com/ujeat/thymeleaflearn/controller/DispatcherController.java
@@ -2,15 +2,12 @@ package com.ujeat.thymeleaflearn.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class DispatcherController {
-    @GetMapping("/index")
-    public String index(){
-        return "/index";
-    }
-    @GetMapping("/footer")
-    public String footer(){
-        return "/footer";
+    @GetMapping("/{view}")
+    public String index(@PathVariable("view")String view){
+        return "/"+view;
     }
 }
